becbc18a7ccf22140ca9bccdf3322d42966c7c74
Andre Mueller <foss@andremueller-online.de>
2016-09-12 20:57:37 +0200

    *

--- src/config.h
+++ src/config.h
@@ -1,3 +1,32 @@
+/*****************************************************************************
+ *
+ * MetaCache - Meta-Genomic Classification Tool
+ *
+ * version 1.0
+ *
+ * Copyright (C) 2016 André Müller (muellan@uni-mainz.de)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *****************************************************************************/
+
+/*****************************************************************************
+ *
+ * @file contains important type aliases
+ *
+ *****************************************************************************/
+
 #ifndef MC_CONFIG_H_
 #define MC_CONFIG_H_
 
@@ -8,11 +37,6 @@
 
 namespace mc {
 
-/*****************************************************************************
- *
- *
- *
- *****************************************************************************/
 using sequence = std::string;
 
 using sketcher = single_function_min_hasher; //default, for 0 <= k <= 32
--- src/sequence_io.cpp
+++ src/sequence_io.cpp
@@ -19,7 +19,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * Original FASTA / FASTQ reaser code taken from the Kraken library written by
+ * Original FASTA / FASTQ reader code taken from Kraken written by
  * Derrick Wood <dwood@cs.jhu.edu>
  *
  *****************************************************************************/
