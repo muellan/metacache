3a7b46c7dd9671e52dffc91c8e1cbd0a113f699e
Andre Mueller <foss@andremueller-online.de>
2018-03-02 10:15:35 +0100

    updated file headers

--- src/args_parser.h
+++ src/args_parser.h
@@ -3,7 +3,7 @@
  *
  * MetaCache - Meta-Genomic Classification Tool
  *
- * Copyright (C) 2016-2017 André Müller (muellan@uni-mainz.de)
+ * Copyright (C) 2016-2018 André Müller (muellan@uni-mainz.de)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
