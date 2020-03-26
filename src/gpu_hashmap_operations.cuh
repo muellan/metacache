a0dd2aa3361b2980f96e92277d12879973c034ef
Robin Kobus <kobus@uni-mainz.de>
2020-03-26 15:34:53 +0100

    enable multi warps per block for insert & query

--- src/gpu_hashmap_operations.cuh
+++ src/gpu_hashmap_operations.cuh
@@ -23,36 +23,38 @@ uint32_t unique_sketch(
     Feature2 * sketch,
     uint32_t sketchSize)
 {
-        uint32_t uniqueFeatureCounter = 0;
+    const int warpLane = threadIdx.x % WARPSIZE;
 
-        for(int i=0; i<ITEMS_PER_THREAD; ++i)
-        {
-            // load candidate and successor
-            const Feature kmer = items[i];
-            Feature nextKmer = threadIdx.x ? items[i] : items[(i+1) % ITEMS_PER_THREAD];
-            nextKmer = __shfl_sync(0xFFFFFFFF, nextKmer, threadIdx.x+1);
+    uint32_t uniqueFeatureCounter = 0;
 
-            // find valid candidates
-            const bool predicate = (kmer != Feature(~0)) && (kmer != nextKmer);
+    for(int i=0; i<ITEMS_PER_THREAD; ++i)
+    {
+        // load candidate and successor
+        const Feature kmer = items[i];
+        Feature nextKmer = warpLane ? items[i] : items[(i+1) % ITEMS_PER_THREAD];
+        nextKmer = __shfl_sync(0xFFFFFFFF, nextKmer, warpLane+1);
 
-            const uint32_t mask = __ballot_sync(0xFFFFFFFF, predicate);
-            const uint32_t count = __popc(mask);
+        // find valid candidates
+        const bool predicate = (kmer != Feature(~0)) && (kmer != nextKmer);
 
-            const uint32_t numPre = __popc(mask & ((1 << threadIdx.x) -1));
-            uint32_t sketchPos = uniqueFeatureCounter + numPre;
+        const uint32_t mask = __ballot_sync(0xFFFFFFFF, predicate);
+        const uint32_t count = __popc(mask);
 
-            // write features to shared memory
-            if(predicate && (sketchPos < sketchSize)) {
-                sketch[sketchPos] = kmer;
-                // printf("sketchPos: %2d feature: %d\n", sketchPos, kmer);
-            }
+        const uint32_t numPre = __popc(mask & ((1 << warpLane) -1));
+        uint32_t sketchPos = uniqueFeatureCounter + numPre;
 
-            uniqueFeatureCounter += count;
+        // write features to shared memory
+        if(predicate && (sketchPos < sketchSize)) {
+            sketch[sketchPos] = kmer;
+            // printf("sketchPos: %2d feature: %d\n", sketchPos, kmer);
         }
-        // cap counter
-        uniqueFeatureCounter = min(uniqueFeatureCounter, sketchSize);
 
-        return uniqueFeatureCounter;
+        uniqueFeatureCounter += count;
+    }
+    // cap counter
+    uniqueFeatureCounter = min(uniqueFeatureCounter, sketchSize);
+
+    return uniqueFeatureCounter;
 }
 
 
@@ -78,7 +80,7 @@ void insert_into_hashtable(
     const auto group =
         cg::tiled_partition<Hashtable::cg_size()>(cg::this_thread_block());
 
-    for(uint32_t i = threadIdx.x; i < sketchCounter*Hashtable::cg_size(); i += WARPSIZE) {
+    for(uint32_t i = (threadIdx.x % WARPSIZE); i < sketchCounter*Hashtable::cg_size(); i += WARPSIZE) {
         const uint8_t gid = i / Hashtable::cg_size();
 
         const auto status = hashtable.insert(
@@ -109,7 +111,7 @@ void query_hashtable(
     const auto group =
         cg::tiled_partition<Hashtable::cg_size()>(cg::this_thread_block());
 
-    for(uint32_t i = threadIdx.x; i < sketchCounter*Hashtable::cg_size(); i += WARPSIZE) {
+    for(uint32_t i = (threadIdx.x % WARPSIZE); i < sketchCounter*Hashtable::cg_size(); i += WARPSIZE) {
         const uint8_t gid = i / Hashtable::cg_size();
         typename Hashtable::value_type valuesOffset = 0;
         //if key not found valuesOffset stays 0
@@ -153,7 +155,7 @@ uint32_t copy_loctions(
         bucketOffset >>= sizeof(bucket_size_type)*CHAR_BIT;
 
         //copy locations
-        for(uint32_t i = threadIdx.x; i < bucketSize; i += WARPSIZE) {
+        for(uint32_t i = (threadIdx.x % WARPSIZE); i < bucketSize; i += WARPSIZE) {
             out[totalLocations + i] = locations[bucketOffset + i];
         }
 
@@ -169,12 +171,12 @@ template<class K>
 __device__ __inline__
 void warp_sort_128(K rg_k[4])
 {
-    const int tid = (threadIdx.x & 31);
-    const int bit1 = (tid>>0)&0x1;
-    const int bit2 = (tid>>1)&0x1;
-    const int bit3 = (tid>>2)&0x1;
-    const int bit4 = (tid>>3)&0x1;
-    const int bit5 = (tid>>4)&0x1;
+    const int warpLane = threadIdx.x % WARPSIZE;
+    const int bit1 = (warpLane>>0)&0x1;
+    const int bit2 = (warpLane>>1)&0x1;
+    const int bit3 = (warpLane>>2)&0x1;
+    const int bit4 = (warpLane>>3)&0x1;
+    const int bit5 = (warpLane>>4)&0x1;
 
     // sort 128 elements
     // exch_intxn: switch to exch_local()
@@ -263,37 +265,36 @@ void warp_sort_128(K rg_k[4])
     CMP_SWP_KEY(K,rg_k[2] ,rg_k[3] );
 
     // transpose blocked to striped
-    const int lane_id = threadIdx.x;
-    if(lane_id&0x1 ) SWP_KEY(K, rg_k[0] , rg_k[1] );
-    if(lane_id&0x1 ) SWP_KEY(K, rg_k[2] , rg_k[3] );
+    if(warpLane&0x1 ) SWP_KEY(K, rg_k[0] , rg_k[1] );
+    if(warpLane&0x1 ) SWP_KEY(K, rg_k[2] , rg_k[3] );
     rg_k[1]  = __shfl_xor_sync(0xffffffff,rg_k[1] , 0x1 );
     rg_k[3]  = __shfl_xor_sync(0xffffffff,rg_k[3] , 0x1 );
-    if(lane_id&0x1 ) SWP_KEY(K, rg_k[0] , rg_k[1] );
-    if(lane_id&0x1 ) SWP_KEY(K, rg_k[2] , rg_k[3] );
-    if(lane_id&0x2 ) SWP_KEY(K, rg_k[0] , rg_k[2] );
-    if(lane_id&0x2 ) SWP_KEY(K, rg_k[1] , rg_k[3] );
+    if(warpLane&0x1 ) SWP_KEY(K, rg_k[0] , rg_k[1] );
+    if(warpLane&0x1 ) SWP_KEY(K, rg_k[2] , rg_k[3] );
+    if(warpLane&0x2 ) SWP_KEY(K, rg_k[0] , rg_k[2] );
+    if(warpLane&0x2 ) SWP_KEY(K, rg_k[1] , rg_k[3] );
     rg_k[2]  = __shfl_xor_sync(0xffffffff,rg_k[2] , 0x2 );
     rg_k[3]  = __shfl_xor_sync(0xffffffff,rg_k[3] , 0x2 );
-    if(lane_id&0x2 ) SWP_KEY(K, rg_k[0] , rg_k[2] );
-    if(lane_id&0x2 ) SWP_KEY(K, rg_k[1] , rg_k[3] );
-    if(lane_id&0x4 ) SWP_KEY(K, rg_k[0] , rg_k[1] );
-    if(lane_id&0x4 ) SWP_KEY(K, rg_k[2] , rg_k[3] );
+    if(warpLane&0x2 ) SWP_KEY(K, rg_k[0] , rg_k[2] );
+    if(warpLane&0x2 ) SWP_KEY(K, rg_k[1] , rg_k[3] );
+    if(warpLane&0x4 ) SWP_KEY(K, rg_k[0] , rg_k[1] );
+    if(warpLane&0x4 ) SWP_KEY(K, rg_k[2] , rg_k[3] );
     rg_k[1]  = __shfl_xor_sync(0xffffffff,rg_k[1] , 0x4 );
     rg_k[3]  = __shfl_xor_sync(0xffffffff,rg_k[3] , 0x4 );
-    if(lane_id&0x4 ) SWP_KEY(K, rg_k[0] , rg_k[1] );
-    if(lane_id&0x4 ) SWP_KEY(K, rg_k[2] , rg_k[3] );
-    if(lane_id&0x8 ) SWP_KEY(K, rg_k[0] , rg_k[2] );
-    if(lane_id&0x8 ) SWP_KEY(K, rg_k[1] , rg_k[3] );
+    if(warpLane&0x4 ) SWP_KEY(K, rg_k[0] , rg_k[1] );
+    if(warpLane&0x4 ) SWP_KEY(K, rg_k[2] , rg_k[3] );
+    if(warpLane&0x8 ) SWP_KEY(K, rg_k[0] , rg_k[2] );
+    if(warpLane&0x8 ) SWP_KEY(K, rg_k[1] , rg_k[3] );
     rg_k[2]  = __shfl_xor_sync(0xffffffff,rg_k[2] , 0x8 );
     rg_k[3]  = __shfl_xor_sync(0xffffffff,rg_k[3] , 0x8 );
-    if(lane_id&0x8 ) SWP_KEY(K, rg_k[0] , rg_k[2] );
-    if(lane_id&0x8 ) SWP_KEY(K, rg_k[1] , rg_k[3] );
-    if(lane_id&0x10) SWP_KEY(K, rg_k[0] , rg_k[1] );
-    if(lane_id&0x10) SWP_KEY(K, rg_k[2] , rg_k[3] );
+    if(warpLane&0x8 ) SWP_KEY(K, rg_k[0] , rg_k[2] );
+    if(warpLane&0x8 ) SWP_KEY(K, rg_k[1] , rg_k[3] );
+    if(warpLane&0x10) SWP_KEY(K, rg_k[0] , rg_k[1] );
+    if(warpLane&0x10) SWP_KEY(K, rg_k[2] , rg_k[3] );
     rg_k[1]  = __shfl_xor_sync(0xffffffff,rg_k[1] , 0x10);
     rg_k[3]  = __shfl_xor_sync(0xffffffff,rg_k[3] , 0x10);
-    if(lane_id&0x10) SWP_KEY(K, rg_k[0] , rg_k[1] );
-    if(lane_id&0x10) SWP_KEY(K, rg_k[2] , rg_k[3] );
+    if(warpLane&0x10) SWP_KEY(K, rg_k[0] , rg_k[1] );
+    if(warpLane&0x10) SWP_KEY(K, rg_k[2] , rg_k[3] );
 
     SWP_KEY(K, rg_k[1] , rg_k[2] );
 }
@@ -316,18 +317,20 @@ void warp_kmerize(
     feature items[4]
 )
 {
+    const int warpLane = threadIdx.x % WARPSIZE;
+
     constexpr int subWarpSize = 4;
-    const int lane = subWarpSize-1 - threadIdx.x % subWarpSize;
+    const int lane = subWarpSize-1 - warpLane % subWarpSize;
 
     uint32_t seq = 0;
     uint32_t ambig = 0;
 
     constexpr int charsPerThread = 4;
     constexpr int charsPerSubWarp = subWarpSize*charsPerThread;
-    char4 chars = reinterpret_cast<const char4*>(sequenceBegin)[threadIdx.x];
+    char4 chars = reinterpret_cast<const char4*>(sequenceBegin)[warpLane];
 
     char c;
-    c = (charsPerThread*threadIdx.x+0 < sequenceLength) ? chars.x : 'N';
+    c = (charsPerThread*warpLane+0 < sequenceLength) ? chars.x : 'N';
     c &= 0b11011111; // to upper case
 
     seq |= (c == 'A') ? 0 : 0;
@@ -339,7 +342,7 @@ void warp_kmerize(
     seq   <<= 2;
     ambig <<= 1;
 
-    c = (charsPerThread*threadIdx.x+1 < sequenceLength) ? chars.y : 'N';
+    c = (charsPerThread*warpLane+1 < sequenceLength) ? chars.y : 'N';
     c &= 0b11011111; // to upper case
 
     seq |= (c == 'A') ? 0 : 0;
@@ -351,7 +354,7 @@ void warp_kmerize(
     seq   <<= 2;
     ambig <<= 1;
 
-    c = (charsPerThread*threadIdx.x+2 < sequenceLength) ? chars.z : 'N';
+    c = (charsPerThread*warpLane+2 < sequenceLength) ? chars.z : 'N';
     c &= 0b11011111; // to upper case
 
     seq |= (c == 'A') ? 0 : 0;
@@ -363,7 +366,7 @@ void warp_kmerize(
     seq   <<= 2;
     ambig <<= 1;
 
-    c = (charsPerThread*threadIdx.x+3 < sequenceLength) ? chars.w : 'N';
+    c = (charsPerThread*warpLane+3 < sequenceLength) ? chars.w : 'N';
     c &= 0b11011111; // to upper case
 
     seq |= (c == 'A') ? 0 : 0;
@@ -387,11 +390,11 @@ void warp_kmerize(
 
     uint32_t ambigStore = __shfl_down_sync(0xFFFFFFFF, ambig, subWarpSize);
     // bounds checking: last subwarp adds ~0
-    ambigStore = (threadIdx.x < WARPSIZE - subWarpSize) ? ambigStore : uint32_t(~0);
+    ambigStore = (warpLane < WARPSIZE - subWarpSize) ? ambigStore : uint32_t(~0);
     ambigStore = (ambig << 16) | ambigStore;
 
     // if(lane == 0) {
-        // const int subwarpId = threadIdx.x / subWarpSize;
+        // const int subwarpId = warpLane / subWarpSize;
         // printf("id: %d seq: %u ambig: %u\n", subwarpId, seq, ambig);
     // }
 
@@ -404,9 +407,9 @@ void warp_kmerize(
 
     // each thread extracts one feature
     for(int i = 0; i < 4; ++i) {
-        const uint8_t seqSlot  = (2*i + threadIdx.x / charsPerSubWarp);
+        const uint8_t seqSlot  = (2*i + warpLane / charsPerSubWarp);
         const uint8_t seqSrc   = seqSlot * 4;
-        const uint8_t kmerSlot = threadIdx.x % charsPerSubWarp;
+        const uint8_t kmerSlot = warpLane % charsPerSubWarp;
 
         uint32_t ambig = __shfl_sync(0xFFFFFFFF, ambigStore, seqSrc);
         // shift to highest bits
@@ -462,9 +465,11 @@ void insert_features(
     using index_type = IndexT;
     using size_type = SizeT;
 
+    constexpr int warpsPerBlock = BLOCK_THREADS / WARPSIZE;
+
     __shared__ union {
         //TODO MAX_SKETCH_SIZE
-        feature sketch[16];
+        feature sketch[warpsPerBlock*16];
     } tempStorage;
 
     for(index_type tgt = blockIdx.y; tgt < numTargets; tgt += gridDim.y) {
@@ -472,8 +477,10 @@ void insert_features(
         const window_id lastWindowSize = sequenceLength % windowStride;
         const window_id numWindows = sequenceLength / windowStride + (lastWindowSize >= kmerSize);
 
+        const int warpId = threadIdx.x / WARPSIZE;
+
         //each block processes one window
-        for(window_id win = blockIdx.x; win < numWindows; win += gridDim.x)
+        for(window_id win = blockIdx.x * warpsPerBlock + warpId; win < numWindows; win += warpsPerBlock * gridDim.x)
         {
             const size_type offsetBegin = sequenceOffsets[tgt] + win*windowStride;
             const size_type offsetEnd = min(sequenceOffsets[tgt+1], offsetBegin + windowSize);
@@ -486,14 +493,14 @@ void insert_features(
             warp_sort_128(items);
 
             uint32_t sketchCounter =
-                unique_sketch<ITEMS_PER_THREAD>(items, tempStorage.sketch, maxSketchSize);
+                unique_sketch<ITEMS_PER_THREAD>(items, tempStorage.sketch+warpId*16, maxSketchSize);
 
             __syncwarp();
 
             const target_id targetId = targetIds[tgt];
             const window_id winOffset = winOffsets[tgt]+win ;
 
-            insert_into_hashtable(hashtable, tempStorage.sketch, sketchCounter, targetId, winOffset);
+            insert_into_hashtable(hashtable, tempStorage.sketch+warpId*16, sketchCounter, targetId, winOffset);
 
             __syncwarp();
         }
@@ -532,13 +539,18 @@ void gpu_hahstable_query(
     using location = Location;
     using bucket_size_type = BucketSizeT;
 
+    constexpr int warpsPerBlock = BLOCK_THREADS / WARPSIZE;
+
     __shared__ union {
         //TODO MAX_SKETCH_SIZE
-        uint64_t sketch[16];
+        uint64_t sketch[warpsPerBlock*16];
     } tempStorage;
 
+    const int warpId = threadIdx.x / WARPSIZE;
+    const int warpLane = threadIdx.x % WARPSIZE;
+
     //each block processes one query (= one window)
-    for(int queryId = blockIdx.x; queryId < numQueries; queryId += gridDim.x) {
+    for(int queryId = blockIdx.x * warpsPerBlock + warpId; queryId < numQueries; queryId += warpsPerBlock * gridDim.x) {
         const size_type sequenceLength = sequenceOffsets[queryId+1] - sequenceOffsets[queryId];
 
         //only process non-empty queries
@@ -551,20 +563,20 @@ void gpu_hahstable_query(
             warp_sort_128(items);
 
             uint32_t realSketchSize =
-                unique_sketch<ITEMS_PER_THREAD>(items, tempStorage.sketch, maxSketchSize);
+                unique_sketch<ITEMS_PER_THREAD>(items, tempStorage.sketch+warpId*16, maxSketchSize);
 
             __syncwarp();
 
-            query_hashtable(hashtable, tempStorage.sketch, realSketchSize);
+            query_hashtable(hashtable, tempStorage.sketch+warpId*16, realSketchSize);
 
             __syncwarp();
 
             location * out = locations_out + queryId*maxSketchSize*maxLocationsPerFeature;
 
             uint32_t numLocations = copy_loctions(
-                tempStorage.sketch, realSketchSize, locations, maxLocationsPerFeature, out);
+                tempStorage.sketch+warpId*16, realSketchSize, locations, maxLocationsPerFeature, out);
 
-            if(threadIdx.x == 0) {
+            if(warpLane == 0) {
                 //store number of results of this query
                 locationCounts[queryId] = numLocations;
             }
@@ -572,7 +584,7 @@ void gpu_hahstable_query(
             __syncwarp();
         }
         else {
-            if(threadIdx.x == 0) {
+            if(warpLane == 0) {
                 //store number of results of this query
                 locationCounts[queryId] = 0;
             }
