072d7975140ede57c909148d5eca4fbcdf335d3e
Robin Kobus <kobus@uni-mainz.de>
2020-06-19 10:24:31 +0200

    removed unused code

--- src/config.h
+++ src/config.h
@@ -98,9 +98,7 @@ using query_id = std::uint_least64_t;
  */
 using sketching_hash = same_size_hash<kmer_type>;
 
-//using sketcher = single_function_min_hasher<kmer_type,sketching_hash>;
 using sketcher = single_function_unique_min_hasher<kmer_type,sketching_hash>;
-// using sketcher = single_function_hasher<kmer_type,sketching_hash>;
 
 using sketch_size_type = typename sketcher::sketch_type::size_type;
 
