muellan/metacache

commit fbd0ba02a5d538917e0dd3315b11920337786f77

wrapped mutex in lock

src/gpu_hashmap.cu
@@ -540,10 +540,11 @@ private:
         std::mutex& retrieveMtx,
         std::mutex& writeMtx
     ) {
-        retrieveMtx.lock();
-        size_type valuesCount = retrieve_batch(
-            d_keys, buffer, hostId, batchSize, maxLocationsPerFeature);
-        retrieveMtx.unlock();
+        {
+            std::lock_guard<std::mutex> lock(retrieveMtx);
+            size_type valuesCount = retrieve_batch(
+                d_keys, buffer, hostId, batchSize, maxLocationsPerFeature);
+        }
 
         std::lock_guard<std::mutex> lock(writeMtx);
         write_batch(os, buffer, hostId, batchSize, valuesCount);

commit f068423c2e935589a118ba2face7e7fb07f64ba0

fix bug introduced in fbd0ba0

src/gpu_hashmap.cu
@@ -540,9 +540,10 @@ private:
         std::mutex& retrieveMtx,
         std::mutex& writeMtx
     ) {
+        size_type valuesCount = 0;
         {
             std::lock_guard<std::mutex> lock(retrieveMtx);
-            size_type valuesCount = retrieve_batch(
+            valuesCount = retrieve_batch(
                 d_keys, buffer, hostId, batchSize, maxLocationsPerFeature);
         }
 