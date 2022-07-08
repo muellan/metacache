532a737ffca173e264a28b8349a8da98cbf0aeb6
Robin Kobus <kobus@uni-mainz.de>
2022-07-08 18:47:52 +0200

    bumped version

--- src/version.h
+++ src/version.h
@@ -24,11 +24,11 @@
 #define MC_VERSION_H_
 
 
-#define MC_VERSION 20220408
+#define MC_VERSION 20220708
 
 #define MC_DB_VERSION 20200820
 
-#define MC_VERSION_STRING "2.2.2"
+#define MC_VERSION_STRING "2.2.3"
 
 
 #endif
